6c93a18a846bd223e426057919eade4b0954a20d
Yczar <babalolagbogo@gmail.com>
2021-05-13 18:13:32 +0100

    Created a colors.dart file that contains the list of colors that would be used all through the application

---
+++ lib/src/res/values/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+///General [Colors]
+const Color kColorAppBlack = Color(0xFF0F0627);
+const Color kColorAppGrey = Color(0xFF9A96A4);
+const Color kColorAltStroke = Color(0xFFF8F8F9);
+
+///Natural [Colors]
+const Color kColorWhite = Color(0xFFFFFFFF);
+const Color kColorBlack = Color(0xFF000000);
