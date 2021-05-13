cfe2e8e13672c7b06b3972eb3c01a3126d04eb6e
Yczar <babalolagbogo@gmail.com>
2021-05-13 20:33:42 +0100

    wrapped the material application with the scale aware responsible for the app's size configuration

--- lib/xafe.dart
+++ lib/xafe.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:xafe/app/splash_screen/presentation/screens/splash_screen.dart';
 import 'package:xafe/src/res/res.dart';
+import 'package:xafe/src/utils/scaler/scaler.dart';
 
 class Xafe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Xafe',
-      theme: xafeTheme(context),
-      home: SplashScreen(),
+    return ScaleAware(
+      config: const ScaleConfig(),
+      child: MaterialApp(
+        title: 'Xafe',
+        theme: xafeTheme(context),
+        home: SplashScreen(),
+      ),
     );
   }
 }
