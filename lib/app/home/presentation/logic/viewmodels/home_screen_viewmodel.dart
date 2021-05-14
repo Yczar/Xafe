7d2c1820ba381a2d85dde825d27905f0fbf6dadd
Yczar <babalolagbogo@gmail.com>
2021-05-14 10:08:52 +0100

    Modified the show bottom sheet function on the home screen viewmodel

--- lib/app/home/presentation/logic/viewmodels/home_screen_viewmodel.dart
+++ lib/app/home/presentation/logic/viewmodels/home_screen_viewmodel.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:xafe/src/res/res.dart';
+import 'package:xafe/src/utils/scaler/scaler.dart';
 
 class HomeScreenViewModel extends ChangeNotifier {
   void showBottomSheet(BuildContext context) {
@@ -8,7 +10,28 @@ class HomeScreenViewModel extends ChangeNotifier {
         backgroundColor: Colors.transparent,
         context: context,
         builder: (BuildContext context) {
-
+          return Container(
+            decoration: const BoxDecoration(
+              color: kColorWhite,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  height: context.scaleY(83.99),
+                  width: context.scaleY(70),
+                  decoration: BoxDecoration(
+                    color: kColorWhite,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                const YMargin(20),
+              ],
+            ),
+          );
         });
   }
 }
