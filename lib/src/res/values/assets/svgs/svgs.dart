0f0d580b61e01957f932f3625b753ccadb3c6fa5
Yczar <babalolagbogo@gmail.com>
2021-05-14 09:58:22 +0100

    Registered the pen icon inside the list of SVGs

--- lib/src/res/values/assets/svgs/svgs.dart
+++ lib/src/res/values/assets/svgs/svgs.dart
@@ -1,6 +1,7 @@
 /// [SVG's Directories]
 const String _svgDir = 'assets/svgs';
 const String _mainScreenSvgDir = '$_svgDir/main_screen';
+const String _homeScreenSvgDir = '$_svgDir/home_screen';
 
 /**
  * [MainScreen svg's]
@@ -10,3 +11,9 @@ const String _mainScreenSvgDir = '$_svgDir/main_screen';
 const String kHomeIcon = '$_mainScreenSvgDir/home_icon.svg';
 const String kBudgetIcon = '$_mainScreenSvgDir/budget_icon.svg';
 const String kCategoryIcon = '$_mainScreenSvgDir/category_icon.svg';
+/**
+ * [HomeScreen svg's]
+ */
+
+///
+const String kPenIcon = '$_mainScreenSvgDir/pen_icon.svg';
