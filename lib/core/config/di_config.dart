b40c0e21df22764a812ce8554c0b25175a4e0ad4
Yczar <babalolagbogo@gmail.com>
2021-05-15 00:01:28 +0100

    Created the login viewmodel and added the login function

--- lib/core/config/di_config.dart
+++ lib/core/config/di_config.dart
@@ -1,4 +1,3 @@
-
 import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:get_it/get_it.dart';
 import 'package:xafe/app/authentication/authentication_dependencies.dart';
@@ -7,7 +6,6 @@ import 'package:xafe/core/network_checker/network_info.dart';
 import 'firebase/services/auth_service.dart';
 import 'firebase/services/cloud_storage_service.dart';
 import 'firebase/services/firestore_service.dart';
-
 
 GetIt locator = GetIt.I;
 void setupLocator() {
@@ -20,7 +18,6 @@ void setupLocator() {
   locator.registerLazySingleton(() => DataConnectionChecker());
 
   ///[src]
-
 
   ///[App]
   registerAuthenticationDIs();
