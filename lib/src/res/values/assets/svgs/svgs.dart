80b64a1b86e6650f2bafa522eaf8b161b5c7c87f
Yczar <babalolagbogo@gmail.com>
2021-05-14 03:39:04 +0100

    Registered the main screen svgs in the svgs list

---
+++ lib/src/res/values/assets/svgs/svgs.dart
@@ -0,0 +1,12 @@
+/// [SVG's Directories]
+const String _svgDir = 'assets/svgs';
+const String _mainScreenSvgDir = '$_svgDir/main_screen';
+
+/**
+ * [MainScreen svg's]
+ */
+
+///
+const String kHomeIcon = '$_mainScreenSvgDir/home_icon.svg';
+const String kBudgetIcon = '$_mainScreenSvgDir/budget_icon.svg';
+const String kCategoryIcon = '$_mainScreenSvgDir/category_icon.svg';
