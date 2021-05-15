be79f3f617f6b72e5580c3efd04e549f8f04b028
Yczar <babalolagbogo@gmail.com>
2021-05-15 14:29:57 +0100

    Modified the add an expense screen

--- lib/app/home/presentation/screens/add_an_expense_screen.dart
+++ lib/app/home/presentation/screens/add_an_expense_screen.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:xafe/app/categories/data/model/category_model.dart';
+import 'package:xafe/app/categories/domain/usecases/listen_to_categories.dart';
+import 'package:xafe/core/config/di_config.dart';
 import 'package:xafe/src/res/components/buttons/src/xafe_button.dart';
 import 'package:xafe/src/res/res.dart';
 import 'package:xafe/src/utils/navigation/navigation.dart';
@@ -34,50 +38,89 @@ class _AddAnExpenseScreenState extends State<AddAnExpenseScreen> {
                 ),
               ),
               const YMargin(60.45),
-              const Text(
-                'Add an expense',
-                style: TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 24,
-                  color: kColorAppBlack,
+              StreamBuilder<List<CategoryModel>>(
+                stream: locator<ListenToCategories>().call(
+                  uid: FirebaseAuth.instance.currentUser.uid,
                 ),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data.isNotEmpty) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Add an expense',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 24,
+                              color: kColorAppBlack,
+                            ),
+                          ),
+                          const YMargin(35),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: 'expense amount',
+                            ),
+                          ),
+                          const YMargin(10),
+                          DropdownButtonFormField(
+                            iconEnabledColor: kColorAppBlack,
+                            iconDisabledColor: kColorAppBlack,
+                            items: [
+                              const DropdownMenuItem(
+                                child: Text('Hello'),
+                              ),
+                            ],
+                            decoration: const InputDecoration(
+                              hintText: 'Select category',
+                            ),
+                          ),
+                          const YMargin(10),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: 'expense name',
+                            ),
+                          ),
+                          const YMargin(10),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: 'date (dd/mm/yy)',
+                            ),
+                          ),
+                          const Spacer(),
+                          XafeButton(
+                            onPressed: () {},
+                            text: 'Create Category',
+                          ),
+                          const YMargin(80),
+                        ],
+                      );
+                    } else {
+                      return const Expanded(
+                        child: Center(
+                          child: Text(
+                            'No spending categories added yet',
+                          ),
+                        ),
+                      );
+                    }
+                  } else if (snapshot.hasError) {
+                    return Expanded(
+                      child: Center(
+                        child: Text(
+                          'Error: ${snapshot.error}',
+                        ),
+                      ),
+                    );
+                  } else {
+                    return const Expanded(
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                  }
+                },
               ),
-              const YMargin(35),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'expense amount',
-                ),
-              ),
-              const YMargin(10),
-              DropdownButtonFormField(
-                iconEnabledColor: kColorAppBlack,
-                iconDisabledColor: kColorAppBlack,
-                items: [
-                  const DropdownMenuItem(
-                    child: Text('Hello'),
-                  ),
-                ],
-                decoration: const InputDecoration(
-                  hintText: 'Select category',
-                ),
-              ),
-              const YMargin(10),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'expense name',
-                ),
-              ),
-              const YMargin(10),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'date (dd/mm/yy)',
-                ),
-              ), const Spacer(),
-              XafeButton(
-                onPressed: () {},
-                text: 'Create Category',
-              ),
-              const YMargin(80),
             ],
           ),
         ),
