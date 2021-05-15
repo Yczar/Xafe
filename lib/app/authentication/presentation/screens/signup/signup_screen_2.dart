1a8ed08e8a1b4664e272787ee3a5e81c5c7c5919
Yczar <babalolagbogo@gmail.com>
2021-05-16 00:38:20 +0100

    Modified: signup_screen_2.dart
    Signed-off-by: Yczar <babalolagbogo@gmail.com>

--- lib/app/authentication/presentation/screens/signup/signup_screen_2.dart
+++ lib/app/authentication/presentation/screens/signup/signup_screen_2.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:stacked/stacked.dart';
 import 'package:xafe/app/authentication/data/models/user_model.dart';
-import 'package:xafe/app/authentication/presentation/logic/viewmodels/send_email_viewmodel.dart';
 import 'package:xafe/app/authentication/presentation/screens/signup/signup_screen_3.dart';
 import 'package:xafe/app/authentication/presentation/widgets/sign_up_screen_widget.dart';
+import 'package:xafe/src/res/components/forms/helpers/validator_helper.dart';
 import 'package:xafe/src/utils/navigation/navigation.dart';
 
 class SignUpScreen2 extends StatefulWidget {
@@ -35,34 +34,25 @@ class _SignUpScreen2State extends State<SignUpScreen2> {
 
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<SendEmailViewModel>.reactive(
-      viewModelBuilder: ()=>SendEmailViewModel(),
-      builder: (_, model, __) {
-        return SignUpScreenWidget(
-          title: 'What’s your email?',
-            hintText: 'Enter your email address',
-            buttonTitle: 'Next',
-            controller: _emailEditingController,
-            onPressed: () {
-              // model.sendEmail(
-              //      context: context,
-              //      userModel: UserModel(
-              //        name: widget.userModel.name,
-              //        email: _emailEditingController.text,
-              //      ),
-              //    );
-              navigate(
-                context,
-                SignUpScreen3(
-                  userModel: UserModel(
-                    name: widget.userModel.name,
-                    email: _emailEditingController.text,
-                  ),
-                ),
-              );
-            },
-          );
-      }
+    return SignUpScreenWidget(
+      title: 'What’s your email?',
+      hintText: 'Enter your email address',
+      buttonTitle: 'Next',
+      controller: _emailEditingController,
+      validator: (value) {
+        return XafeFormFieldValidator.emailValidator(value);
+      },
+      onPressed: () {
+        navigate(
+          context,
+          SignUpScreen3(
+            userModel: UserModel(
+              name: widget.userModel.name,
+              email: _emailEditingController.text,
+            ),
+          ),
+        );
+      },
     );
   }
 }
