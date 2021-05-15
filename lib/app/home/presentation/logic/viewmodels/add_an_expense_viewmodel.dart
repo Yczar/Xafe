1e4eba3c9755c1c0439a413a5a497a901ebc1d56
Yczar <babalolagbogo@gmail.com>
2021-05-15 17:03:08 +0100

    Created: AddAnExpenseViewmodel to handle all the logic required for writing an expense to the database
    Signed-off-by: Yczar <babalolagbogo@gmail.com>

---
+++ lib/app/home/presentation/logic/viewmodels/add_an_expense_viewmodel.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:xafe/app/home/data/models/expense_model.dart';
+import 'package:xafe/app/home/domain/usecases/add_an_expense.dart';
+import 'package:xafe/src/utils/navigation/navigation.dart';
+
+class AddAnExpenseViewmodel extends BaseViewModel {
+  AddAnExpenseViewmodel(this._addAnExpense);
+
+  final AddAnExpense _addAnExpense;
+
+  Future addAnExpense({
+    ExpenseModel params,
+    BuildContext context,
+  }) {
+    setBusy(true);
+    return _addAnExpense(params)
+      ..then(
+        (result) {
+          result.fold(
+            (failure) {},
+            (data) {
+              popView(context);
+            },
+          );
+        },
+      );
+  }
+}
