28177c1d726c1d6657bfd2ace71df8ff8c700572
Yczar <babalolagbogo@gmail.com>
2021-05-15 11:21:29 +0100

    Implemented the categories datasource impl

---
+++ lib/app/categories/data/datasource/impl/categories_datasource_impl.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:xafe/app/categories/data/datasource/categories_datasource.dart';
+import 'package:xafe/app/categories/data/model/category_model.dart';
+import 'package:xafe/core/config/firebase/firebase.dart';
+
+@override
+class CategoriesDataSourceImpl extends CategoriesDataSource {
+  CategoriesDataSourceImpl(this.xafeFireStoreService);
+
+  final XafeFireStoreService xafeFireStoreService;
+
+  @override
+  Future<void> createCategory(CategoryModel params) {
+    final response = xafeFireStoreService.createInnerDocument(
+      collectionName: 'users',
+      docId: FirebaseAuth.instance.currentUser.uid,
+      innerCollectionName: 'categories',
+      innerDocId: params.categoryId,
+      data: params.toJson(),
+    );
+    return response;
+  }
+
+  @override
+  Stream<List<CategoryModel>> listenToCategories(String uId) {
+    return xafeFireStoreService
+        .listenToInnerCollection(
+          collectionName: 'users',
+          docId: FirebaseAuth.instance.currentUser.uid,
+          innerCollectionName: 'categories',
+        )
+        .map(
+          (snapshot) => snapshot.docs
+              .map(
+                (docSnapshot) => CategoryModel.fromJson(
+                  docSnapshot.data(),
+                ),
+              )
+              .toList(),
+        );
+  }
+}
